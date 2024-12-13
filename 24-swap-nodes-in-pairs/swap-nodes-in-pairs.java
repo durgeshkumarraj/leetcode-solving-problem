be093a6ba117820a67ac2b69da5e48b537f8b651
durgesh kumar <131584021+durgeshkumarraj@users.noreply.github.com>
2024-12-13 18:29:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (25.66%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -8,21 +8,46 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode swapPairs(ListNode head) {
+//         //base case
+//         if(head == null || head.next ==null){
+//             return head;
+//         }
+//         //created a new NOde;
+
+//         ListNode temp = head.next;
+//         head.next = temp.next;
+//         temp.next = head;
+
+//         head.next = swapPairs(head.next);
+
+//         return temp;
+           
+//     }
+// }
+
+
+
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        //base case
-        if(head == null || head.next ==null){
-            return head;
+        if (head == null || head.next == null) return head;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        while (prev.next != null && prev.next.next != null) {
+            ListNode first = prev.next;
+            ListNode second = prev.next.next;
+
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+
+            prev = first;
         }
-        //created a new NOde;
 
-        ListNode temp = head.next;
-        head.next = temp.next;
-        temp.next = head;
-
-        head.next = swapPairs(head.next);
-
-        return temp;
-           
+        return dummy.next;
     }
 }
